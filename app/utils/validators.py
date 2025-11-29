05e71cc955d76363f4c90042a395d27b5c72cae9
FatemehKeshavarzi <fatemeh.kesha98@gmail.com>
2025-11-29 23:50:10 +0330

    add task validators

--- app/utils/validators.py
+++ app/utils/validators.py
@@ -10,6 +10,17 @@ def validate_project_title(title:str) -> None:
     if len(title) > 30:
             raise ValueError('title must be less than 30 characters')
 
+
 def validate_project_description(description:str):
+    if len(description) > 150:
+            raise ValueError('description must be less that 150 characters')
+
+
+def validate_task_title(title:str) -> None:
+    if len(title) > 30:
+            raise ValueError('title must be less than 30 characters')
+
+
+def validate_task_description(description:str):
     if len(description) > 150:
             raise ValueError('description must be less that 150 characters')
