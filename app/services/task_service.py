68e40807d0776f0b359a7c287b8deff18bd642a1
FatemehKeshavarzi <fatemeh.kesha98@gmail.com>
2025-11-30 00:48:02 +0330

    add task service update task

--- app/services/task_service.py
+++ app/services/task_service.py
@@ -1,11 +1,13 @@
 import os
 from dotenv import load_dotenv
 from typing import Literal
+from datetime import date
 from app.repositories.task_repository import TaskRepository
 from app.models.project_model import Project
 from app.models.task_model import Task
 from app.utils.func import generate_random_id
-from app.utils.validators import validate_task_title, validate_task_description
+from app.utils.validators import validate_task_title, validate_task_description, validate_task_deadline
+from app.utils.func import parse_deadline
 
 load_dotenv()
 
@@ -14,7 +16,7 @@ class TaskService:
     def __init__(self, repo:TaskRepository) -> None:
         self.repo = repo
 
-    def create_task(self, project_id:int, title:str, description:str, deadline:str|None, status:Literal['done', 'doing', 'todo']):
+    def create_task(self, project_id:int, title:str, description:str, deadline:str|None, status:Literal['done', 'doing', 'todo']) -> Task:
         validate_task_title(title=title)
         validate_task_description(description=title)
         if len(self.repo.all()) >= int(os.getenv('MAX_NUMBER_OF_TASK', 0)):
@@ -22,3 +24,11 @@ class TaskService:
         task = Task(task_id=generate_random_id(), project_id=project_id, title=title, description=description, deadline=deadline, status=status)
         return self.repo.create(task=task)
     
+    def update_task(self, task_id:int, title:str, description:str, deadline:str|None, status:Literal['done', 'doing', 'todo']) -> Task:
+        validate_task_title(title=title)
+        validate_task_description(description=description)
+        if deadline:
+            deadline_date : date = parse_deadline(deadline=deadline)
+            validate_task_deadline(deadline=deadline_date)
+        return self.repo.update(task_id=task_id, title=title, description=description, deadline=deadline, status=status)
+            
--- app/utils/validators.py
+++ app/utils/validators.py
@@ -1,14 +1,13 @@
 from datetime import date
 
-def validate_deadline(deadline:date) -> bool:
-    if deadline >= date.today():
-        return True
-    return False
+def validate_task_deadline(deadline:date) -> None:
+    if deadline < date.today():
+        raise ValueError('invalid date')
 
 
 def validate_project_title(title:str) -> None:
     if len(title) > 30:
-            raise ValueError('title must be less than 30 characters')
+        raise ValueError('title must be less than 30 characters')
 
 
 def validate_project_description(description:str):
@@ -18,9 +17,9 @@ def validate_project_description(description:str):
 
 def validate_task_title(title:str) -> None:
     if len(title) > 30:
-            raise ValueError('title must be less than 30 characters')
+        raise ValueError('title must be less than 30 characters')
 
 
 def validate_task_description(description:str):
     if len(description) > 150:
-            raise ValueError('description must be less that 150 characters')
+        raise ValueError('description must be less that 150 characters')
