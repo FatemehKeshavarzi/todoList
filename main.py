f0bd3c1ebe86c6eea9f84e2bd842872cfdca0358
FatemehKeshavarzi <fatemeh.kesha98@gmail.com>
2025-12-06 03:10:03 +0330

    add uvicorn run to main

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
+import uvicorn
 from app.cli import CLIHandler
-
+from app.api.routers import app
 
 if __name__ == '__main__':
 
-    CLIHandler()
+    # CLIHandler()
+    uvicorn.run(app=app, host='127.0.0.1', port=8000)
     pass
