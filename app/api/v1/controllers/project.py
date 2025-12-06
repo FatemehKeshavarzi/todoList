cbc8eb4ec96a71786e5ff0baf26444b2e7a5ff49
FatemehKeshavarzi <fatemeh.kesha98@gmail.com>
2025-12-07 00:04:04 +0330

    update service repos to sql repos

--- app/api/v1/controllers/project.py
+++ app/api/v1/controllers/project.py
@@ -12,7 +12,7 @@ router = APIRouter()
 @router.get('/projects', response_model=ResponseSchema[list[ProjectListSchema]], responses={400: {"model": ErrorSchema}})
 def list_projects():
     try:
-        project_service = ProjectService(project_repo=InMemoryProjectRepository(), task_repo=InMemoryTaskRepository())
+        project_service = ProjectService(project_repo=SQLProjectRepository(), task_repo=SQLTaskRepository())
         projects = project_service.list_projects()
         return ResponseSchema(code=1, data=projects)
     except Exception as e:
@@ -21,9 +21,8 @@ def list_projects():
 
 @router.post('/projects', response_model=ResponseSchema[ProjectCreateResponseSchema], responses={400: {"model": ErrorSchema}})
 def add_project(data: ProjectCreateRequestSchema):
-    # todo : update repos
     try:
-        project_service = ProjectService(project_repo=InMemoryProjectRepository(), task_repo=InMemoryTaskRepository())
+        project_service = ProjectService(project_repo=SQLProjectRepository(), task_repo=SQLTaskRepository())
         project = project_service.create_project(title=data.title, description=data.description)
         return ResponseSchema(code=1, data=project)
     except Exception as e:
@@ -32,9 +31,8 @@ def add_project(data: ProjectCreateRequestSchema):
 
 @router.put('/projects/{project_code}', response_model=ResponseSchema[ProjectUpdateResponseSchema], responses={400: {"model": ErrorSchema}})
 def update_project(project_code:int, data:ProjectUpdateRequestSchema):
-    # todo : update repos
     try:
-        project_service = ProjectService(project_repo=InMemoryProjectRepository(), task_repo=InMemoryTaskRepository())
+        project_service = ProjectService(project_repo=SQLProjectRepository(), task_repo=SQLTaskRepository())
         project = project_service.update_project(project_code=project_code, title=data.title, description=data.description)
         return ResponseSchema(code=1, data=project)
     except Exception as e:
@@ -43,9 +41,8 @@ def update_project(project_code:int, data:ProjectUpdateRequestSchema):
 
 @router.delete('/projects/{project_code}', response_model=ResponseSchema[str], responses={400: {"model": ErrorSchema}})
 def delete_project(project_code:int):
-    # todo: update repos
     try:
-        project_service = ProjectService(project_repo=InMemoryProjectRepository(), task_repo=InMemoryTaskRepository())
+        project_service = ProjectService(project_repo=SQLProjectRepository(), task_repo=SQLTaskRepository())
         project_service.delete_project(project_code=project_code)
         return ResponseSchema(code=1, data='deleted')
     except Exception as e:
--- app/api/v1/controllers/task.py
+++ app/api/v1/controllers/task.py
@@ -1,8 +1,8 @@
 from fastapi import APIRouter
 from app.api.v1.schemas.request import ResponseSchema, ErrorSchema
 from app.api.v1.schemas.task import TaskCreateRequestSchema, TaskCreateResponseSchema, TaskUpdateRequestSchema, TaskUpdateResponseSchema, TaskListSchema
-from app.repositories.task_repository import InMemoryTaskRepository, SQLTaskRepository
-from app.repositories.project_repository import InMemoryProjectRepository, SQLProjectRepository
+from app.repositories.task_repository import SQLTaskRepository, InMemoryTaskRepository
+from app.repositories.project_repository import SQLProjectRepository, InMemoryProjectRepository
 from app.services.project_service import ProjectService
 from app.services.task_service import TaskService
 from app.services.project_service import ProjectService
@@ -14,9 +14,8 @@ router = APIRouter()
 
 @router.get('/projects/{project_code}/tasks', response_model=ResponseSchema[list[TaskListSchema]], responses={400:{'model':ErrorSchema}})
 def list_tasks(project_code:int):
-    # todo: update repos
     try:
-        project_service = ProjectService(project_repo=InMemoryProjectRepository(), task_repo=InMemoryTaskRepository())
+        project_service = ProjectService(project_repo=SQLProjectRepository(), task_repo=SQLTaskRepository())
         tasks = project_service.list_project_tasks(project_code=project_code)
         return ResponseSchema(code=1, data=tasks)
 
@@ -26,9 +25,8 @@ def list_tasks(project_code:int):
 
 @router.post('/projects/{project_code}', response_model=ResponseSchema[TaskCreateResponseSchema], responses={400:{'model':ErrorSchema}})
 def add_task(project_code:int, data:TaskCreateRequestSchema):
-    # todo: update repos
     try:
-        task_service = TaskService(task_repo=InMemoryTaskRepository())
+        task_service = TaskService(task_repo=SQLTaskRepository())
         task = task_service.create_task(project_code=project_code, title=data.title, description=data.description, deadline=data.deadline, status=data.status)
         return ResponseSchema(code=1, data=task)
 
@@ -38,9 +36,8 @@ def add_task(project_code:int, data:TaskCreateRequestSchema):
 
 @router.put('/projects/tasks/{task_code}', response_model=ResponseSchema[TaskUpdateResponseSchema], responses={400:{'model':ErrorSchema}})
 def update_task(task_code:int, data:TaskUpdateRequestSchema):
-    # todo: update repos
     try:
-        task_service = TaskService(task_repo=InMemoryTaskRepository())
+        task_service = TaskService(task_repo=SQLTaskRepository())
         task = task_service.update_task(task_code=task_code, title=data.title, description=data.description, deadline=data.deadline, status=data.status)
         return ResponseSchema(code=1, data=task)
 
@@ -50,9 +47,8 @@ def update_task(task_code:int, data:TaskUpdateRequestSchema):
 
 @router.delete('/projects/tasks/{task_code}', response_model=ResponseSchema[str], responses={400:{'model':ErrorSchema}})
 def delete_task(task_code:int):
-    # todo: update repos
     try:
-        task_service = TaskService(task_repo=InMemoryTaskRepository())
+        task_service = TaskService(task_repo=SQLTaskRepository())
         task_service.delete_task(task_code=task_code)
         return ResponseSchema(code=1, data='deleted')
 
